9917ceb689071037220cfe97a3e0abf0db833fac
RitikVaishnav <88304068+RitikVaishnav@users.noreply.github.com>
2022-08-02 10:55:55 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,48 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+
+const app = express();
+app.use("/assets",express.static("assets"));
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodejs"
+});
+
+// connect to the database
+connection.connect(function(error){
+    if (error) throw error
+    else console.log("connected to the database successfully!")
+});
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/login.html");
+})
+
+app.post("/",encoder, function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("select * from userdata where username = ? and userpwd = ?",[username,password],function(error,results,fields){
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+// when login is success
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
+})
+
+
+// set app port 
+app.listen(4000);
